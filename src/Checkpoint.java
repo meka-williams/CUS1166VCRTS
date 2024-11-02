4bb0a18f1df52a6696e2da69624145693f19f91f
Jeryv <vizhnayjery@gmail.com>
2024-11-01 22:48:16 -0400

    Added Checkpoint & VController files, modified JobRequest

---
+++ src/Checkpoint.java
@@ -0,0 +1,80 @@
+public class Checkpoint {
+    private int checkpointID;
+    private int jobID;
+    private int vehicleID;
+    private Object image;
+    private float percentLeft;
+
+
+    public Checkpoint(int checkpointID, int jobID, int vehicleID){
+        this.checkpointID = checkpointID;
+        this.jobID = jobID;
+        this.vehicleID = vehicleID;
+        this.percentLeft = 100.0f;
+
+    }
+
+    /**
+     * This creates an image object that represents the current state of the Job
+     * @returns Object representing the checkpoint image.
+     */
+    public Object createImage(){
+        return new Object();
+    }
+
+    /*
+     * This copies the checkpoint data to a new vehicle
+     * Vehicle class NEEDS TO BE IMPLEMENTED for this to work
+     */
+    public void copyToNewVehicle(Vehicle vehicle){
+        vehicle.setLastCheckpointID(this.checkpointID);
+    }
+    
+    /*
+     * This retarts the job from this checkpoint
+     */
+    public void restartJob(){
+        this.percentLeft = 100.0f;
+    }
+    
+    //Getters and Setters
+    public int getCheckpointID(){
+        return checkpointID;
+    }
+
+    public void setCheckpointID(int checkpointID){
+        this.checkpointID = checkpointID;
+    }
+
+    public int getJobID(){
+        return jobID;
+    }
+
+    public void setJobID(int jobID){
+        this.jobID = jobID;
+    }
+
+    public int getVehicleID(){
+        return vehicleID;
+    }
+
+    public void setVehicleID(int vechileID){
+        this.vehicleID = vechileID;
+    }
+
+    public float getPercentLeft(){
+        return percentLeft;
+    }
+
+    public void setPercentLeft(float percentLeft){
+        this.percentLeft = percentLeft;
+    }
+
+    public Object getImage(){
+        return image;
+    }
+
+    public void setImage(Object image){
+        this.image = image;
+    }
+}
